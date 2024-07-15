9288a9b830d772335a7efb160aa9135e09c664d4
Aarya Rashinkar <75886860+Aarya3624@users.noreply.github.com>
2024-07-15 12:26:02 +0530

    First commit

---
+++ src/app/components/anpr.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ANPR = () => {
+  return <h1 className="text-3xl font-bold">ANPR</h1>;
+};
+
+export default ANPR;
---
+++ src/app/components/people-counter.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const PeopleCounter = () => {
+  return <h1 className="text-3xl font-bold">People Counter</h1>;
+};
+
+export default PeopleCounter;
